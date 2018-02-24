2aef65fa5526bf6255c0d2d5e79751ccc4e0d722
Juan Catalan <jcatalanolmos@gmail.com>
2018-02-24 16:32:31 -0300

    replace base provision with snapshot image

--- provision.tf
+++ provision.tf
@@ -7,30 +7,6 @@ resource "null_resource" "provisioner" {
 
     provisioner "remote-exec" {
       inline = [
-        "yum update -y",
-        "yum install -y wget ccze fail2ban logwatch git"
-      ]
-    }
-
-    provisioner "remote-exec" {
-      inline = [
-        "cd /tmp",
-        "wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm",
-        "rpm -i epel-release-latest-7.noarch.rpm",
-        "yum update -y",
-      ]
-    }
-
-    provisioner "remote-exec" {
-      inline = [
-       "yum install -y python python-pip",
-       "pip install -U pip",
-       "pip install -U ansible" 
-      ]
-    }
-   
-    provisioner "remote-exec" {
-      inline = [
         "cd /tmp",
         "git clone https://github.com/jscatala/ansible_docker_rhel",
         "cd ansible-role-docker",
